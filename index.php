0e9e2273f5d87c4d7af0aa2bc38e1f4183a60f15
githubheang01 <theang2@paragoniu.edu.kh>
2023-08-23 12:29:43 +0700

    put footer in footer.php

--- index.php
+++ index.php
@@ -59,46 +59,7 @@
     <!-- Clsoe Main Body Container -->
 
     <!-- Put Footer Below This -->
-    <div class="footer">
-        <div class="footer-top">
-
-            <!-- logo -->
-            <div class="footer-link-wrapper">
-                <div class="footer-header">Ear Candy</div>
-                <img class="ear-candy-logo" src="images/logo.png" alt="">
-            </div>
-            <!-- end logo -->
-
-            <!-- product -->
-            <div class="footer-link-wrapper">
-                <div class="footer-header">Products</div>
-                <a class="footer-link" href="#">Piano</a>
-                <a class="footer-link" href="#">Guitar</a>
-                <a class="footer-link" href="#">Violin</a>
-            </div>
-            <!-- end product -->
-
-            <!-- about -->
-            <div class="footer-link-wrapper">
-                <div class="footer-header">About</div>
-                <a class="footer-link" href="#">Our Team</a>
-                <a class="footer-link" href="#">Features</a>
-            </div>
-            <!-- end about -->
-
-            <!-- contact us -->
-            <div class="footer-link-wrapper">
-                <div class="footer-header">Contact Us</div>
-
-                <a class="footer-link-img" href="#"><img src="images/footer_image/facebook.png"></a>
-                <a class="footer-link-img" href="#"><img src="images/footer_image/instagram.png"></a>
-                <a class="footer-link-img" href="#"><img src="images/footer_image/telegram.png"></a>
-
-            </div>
-            <!-- end contact us -->
-        </div>
-        <div class="footer-bottom">EarCandy co.ltd. 2023</div>
-    </div>
+    <?php include("layout/footer.php") ?>
     <!-- Footer End -->
 </body>
 
