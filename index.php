d6e8ad7f1542a1505e741c132bf388503b4b5508
githubheang01 <theang2@paragoniu.edu.kh>
2023-08-23 12:19:55 +0700

    put topnav in header.php file

--- index.php
+++ index.php
@@ -13,26 +13,7 @@
 
 <body>
     <!-- Open Navigation Bar Container -->
-    <div class="topnav">
-        <div class="topnav-left">
-            <img class="logo" src="images/logo.png">
-            <a href="#">EAR CANDY</a>
-        </div>
-
-        <div class="topnav-mid">
-            <a id="piano" href="#">Piano</a>
-            <a id="guitar" href="#">Guitar</a>
-            <a id="violin" href="#">Violin</a>
-            <a id="about" href="#">About</a>
-            <a id="support" href="#">Support</a>
-        </div>
-
-        <div class="topnav-right">
-            <a id="shopping-cart" href="#"><img src="images/shopping-cart.png"></a>
-            <a id="sign-in" href="#">Sign In</a>
-            <a id="register" href="#">Register</a>
-        </div>
-    </div>
+    <?php include("layout/header.php") ?>
     <!-- Close Navigation Bar Container -->
 
     <!-- Open Main Body Container -->
