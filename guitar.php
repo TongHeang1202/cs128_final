5d51e86f05efeb234b5b1d1528830d702b30521c
githubheang01 <theang2@paragoniu.edu.kh>
2023-08-27 21:43:58 +0700

    fixed menu css

--- guitar.php
+++ guitar.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="images/logo.png">
     <link href="css/nav.css" rel="stylesheet">
-    <link href="css/piano.css" rel="stylesheet">
+    <link href="css/menu.css" rel="stylesheet">
     <link href="css/footer.css" rel="stylesheet">
     <title>Guitar</title>
 </head>
