1babbc68af09fafbf35de16831b1afcb21682209
kolbotpen <kolbot.pen@gmail.com>
2023-08-30 19:45:50 +0700

    added about

---
+++ about.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="images/logo.png">
+    <link href="css/nav.css" rel="stylesheet">
+    <link href="css/menu.css" rel="stylesheet">
+    <link href="css/footer.css" rel="stylesheet">
+    <title>Guitar</title>
+</head>
+<body>
+    <!-- Open Navigation Bar Container -->
+    <?php include("layout/header.php") ?>
+    <!-- Close Navigation Bar Container -->
+    
+    
+
+    <!-- Put Footer Below This -->
+    <?php include("layout/footer.php") ?>
+    <!-- Footer End -->
+</body>
+</html>
