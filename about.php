0901b23950843566649e13dcd32f97cf9eb0f200
kolbotpen <kolbot.pen@gmail.com>
2023-09-01 16:19:47 +0700

    about.php and master.css changes

--- about.php
+++ about.php
@@ -5,17 +5,36 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="images/logo.png">
     <link href="css/nav.css" rel="stylesheet">
-    <link href="css/menu.css" rel="stylesheet">
+    <link href="css/master.css" rel="stylesheet">
     <link href="css/footer.css" rel="stylesheet">
-    <title>Guitar</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css"> 
+    <link rel="preconnect" href="https://fonts.googleapis.com"> 
+    <link rel="preconnect" href="https: //fonts.gstatic.com" crossorigin> 
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital, wght@0, 100;0 ,200;0,300; 0, 400; 0, 500; 0, 600; 0, 700; 1, 100; 1, 200; 1, 300; 1, 400; 1, 500; 1, 600; 1, 700&display=swap" rel="stylesheet">
+    <title>About EarCandy</title>
 </head>
 <body>
     <!-- Open Navigation Bar Container -->
     <?php include("layout/header.php") ?>
     <!-- Close Navigation Bar Container -->
-    
-    
-
+    <section class="about">
+        <div class="about-main">
+            <img src="images/logo.png">
+            <div class="about-text">
+                <h1>About Us</h1>
+                <h5>To be the tastebuds of your ears</h5>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                    sed do eiusmod tempor incididunt ut labore et dolore magna 
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis 
+                    aute irure dolor in reprehenderit in voluptate velit esse cillum 
+                    dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat 
+                    non proident, sunt in culpa qui officia deserunt mollit anim id est 
+                    laborum.</p>
+                <button type="button">Contact Us</button>   
+            </div>
+        </div>  
+    </section>
     <!-- Put Footer Below This -->
     <?php include("layout/footer.php") ?>
     <!-- Footer End -->
